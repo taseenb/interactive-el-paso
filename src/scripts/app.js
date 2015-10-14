f32be20dde5bebcb171e7281ea9b47d8ae0eea0e
taseenb <esteban.almiron@gmail.com>
2015-10-14 16:32:32 +0100

    update

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,58 +2,64 @@ define( function ( require ) {
 
   'use strict';
 
-  // Create App global
-  window.App = window.App || {};
-
-  // Support
-  App.supportTransitions = !$( 'html' ).hasClass( 'no-csstransitions' );
-
-  // ##################################################
-  // #
-  // Swiper Options
-  // #
-  // ##################################################
-  // #
-  App.swiperLoop = App.supportTransitions ? true : false;
-  // #
-  // ##################################################
+  $( function () {
 
 
-  // Global Events - pub/sub
-  var Mediator = require( 'mediator-js' );
-  App.mediator = new Mediator();
+    // Create App global
+    window.App = window.App || {};
+
+    // Support
+    App.supportTransitions = !$( 'html' ).hasClass( 'no-csstransitions' );
+
+    // ##################################################
+    // #
+    // Swiper Options
+    // #
+    // ##################################################
+    // #
+    App.swiperLoop = App.supportTransitions ? true : false;
+    // #
+    // ##################################################
 
 
-  // Resize event
-  var resizeEvent = require( 'resize' );
-  resizeEvent.initialize();
+    // Global Events - pub/sub
+    var Mediator = require( 'mediator-js' );
+    App.mediator = new Mediator();
 
 
-  // Device
-  App.isTouch = $( 'html' ).hasClass( 'touch' );
-  App.isPhone = App.isTouch && (App.width < 481 || App.height < 481);
+    // Resize event
+    var resizeEvent = require( 'resize' );
+    resizeEvent.initialize();
 
 
-  // Import Swiper
-  App.swiper = require( 'swiper' );
+    // Device
+    App.isTouch = $( 'html' ).hasClass( 'touch' );
+    App.isPhone = App.isTouch && (App.width < 481 || App.height < 481);
 
 
-  // Get data and start main view
-  $.ajax( {
-    url: 'data/data.js',
-    dataType: 'jsonp',
-    jsonpCallback: 'callback',
-    success: function ( data ) {
-      App.data = data;
+    // Import Swiper
+    App.swiper = require( 'swiper' );
 
-      console.log( data );
-      //console.log( App.width, App.height );
 
-      // Main view
-      var MainView = require( 'views/mainView.js' );
-      App.mainView = new MainView( '#main' );
-      App.mainView.render();
-    }
+    // Get data and start main view
+    $.ajax( {
+      url: 'data/data.js',
+      dataType: 'jsonp',
+      jsonpCallback: 'callback',
+      success: function ( data ) {
+        App.data = data;
+
+        console.log( data );
+        //console.log( App.width, App.height );
+
+        // Main view
+        var MainView = require( 'views/mainView.js' );
+        App.mainView = new MainView( '#main' );
+        App.mainView.render();
+      }
+    } );
+
+
   } );
 
 
